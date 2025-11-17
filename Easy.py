591e0a4fd1f96f6b95eb001df0dfcb1b22c8ef7b
Xiaoming0313883 <xiao69227@gmail.com>
2025-11-17 19:05:51 +0800

    upload the easy number counter

---
+++ Easy.py
@@ -0,0 +1,11 @@
+#request user input the initial number to count
+start = int(input("Enter a number to start counting: "))
+
+#loop start with the number that user input
+#loop until the counter become zero
+while start >= 0:
+    print(start) #print the counting number
+    start-=1 #minus 1 after counted
+print("Blaze off!") #when the loop is end,
+                    #mean that counted until
+                    #zero
