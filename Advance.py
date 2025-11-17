5e65952cbba5342fb44770d608ba683790d19a36
Xiaoming0313883 <xiao69227@gmail.com>
2025-11-17 22:27:32 +0800

    can count start from negative numbers

--- Advance.py
+++ Advance.py
@@ -3,17 +3,23 @@ RESET = '\033[0m'
 
 def init():
     start = input("Enter a number to start counting: ")
-    if not start.isdigit():
+    if not start.lstrip("-").isdigit(): #lstrip: remove leading characters from a string
         print(f"{RED}Please enter a number{RESET}")
         init()
     else:
-        countDown(int(start))
+        countDown(int(start.lstrip("-")),getSign(int(start)))
 
-def countDown(number: int):
+def countDown(number: int, sign: str):
     while number >= 0:
-        print(number)
+        print((sign if number != 0 else "") + str(number))
         number -= 1
     print("Blaze off!")
 
+def getSign(number: int):
+    if number < 0:
+        return "-"
+    return ""
+
 init()
+
 
