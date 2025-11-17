c4902816de9c606a94ad01a23a34a5b2d24bf87b
Xiaoming0313883 <xiao69227@gmail.com>
2025-11-17 19:46:49 +0800

    added integer checker

--- Advance.py
+++ Advance.py
@@ -1,7 +1,11 @@
 
 def init():
     start = input("Enter a number to start counting: ")
-    countDown(int(start))
+    if not start.isdigit():
+        print("Please enter a number")
+        init()
+    else:
+        countDown(int(start))
 
 def countDown(number: int):
     while number >= 0:
